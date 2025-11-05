20411587bdce3c4e4888e53b293c272f69dd7917
angelingbusta <angelingbusta@gmail.com>
2025-11-05 15:07:47 -0700

    gg

--- web_scrapping3.py
+++ web_scrapping3.py
@@ -1,40 +1,26 @@
-import streamlit as td
-import  requests 
+import streamlit as st
+import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-
-def get_data(dept):
-    url = requests.get(f'https://www.{dept}.ruet.ac.bd/teacher_list').text
-    soup = BeautifulSoup(url, 'lxml')
-    teachers = soup.find_all('tr')[1:]
-    name_en = []
-    designation = []    
-    phone_no = []
-    email = []
-    dept=[]
-    for teacher in teachers:
-        name = teacher.find_all('td')[1].text.strip()
-        desig = teacher.find_all('td')[3].text.strip()
-        phone = teacher.find_all('td')[6].text.strip()
-        em = teacher.find_all('td')[5].text.strip()
-        dept = teacher.find_all('td')[4].text.strip()
-
-        name_en.append(name)
-        designation.append(desig)
-        phone_no.append(phone)
-        email.append(em)
-        dept.append(dept)
-
-    data = pd.DataFrame({'Name': name_en, 'Designation': designation, 'Phone': phone_no, 'Email': email, 'Department': dept})
-    return data
-def main():
-    td.title("RUET Teacher Information")
-    #department selection
-    dept = td.selectbox ['eee', 'chem', 'math', 'phy', 'chem']
-    dept =  st.sidebar.selectbox("Select Department", dept ).lower()
-
-    if dept:
-        data = get_data(dept)
-        st.dataframe(data)
-
-        #constructor
+ 
+url = requests.get(f'https://www.scrapethissite.com/pages/forms/').text
+soup = BeautifulSoup(url, 'lxml')
+#players = soup.find_all('tr')
+#print(players)
+players = soup.find_all('tr')[1:]
+#st.write(players[0])
+team_name = []
+years = []
+wins = []
+ 
+for i in players:
+    name = i.find_all('td')[0].text.strip()
+    year = i.find_all('td')[1].text.strip()
+    win = i.find_all('td')[2].text.strip()
+    team_name.append(name)
+    years.append(year)
+    wins.append(win)
+ 
+data = pd.DataFrame({'Team Name': team_name, 'Year': years, 'Wins': wins})
+st.dataframe(data)
+ 
